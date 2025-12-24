91ad40a11576b3cc1002b4846ae992b637ed0485
M Atakan Gurkan <ato.gurkan@gmail.com>
2025-12-24 12:02:04 +0300

    added 4th order RK

---
+++ RK_4th_v1.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def Ene(w):
+    x, y, vx, vy = w
+    KE = 1/2*(vx*vx + vy*vy)
+    PE = 1/2*(x*x + y*y)
+    return KE+PE
+
+
+def f(w, t):
+    x, y, vx, vy = w
+    return np.asarray([vx, vy, -x, -y])
+
+# initial conditions
+t = 0
+x, y = 1, 1/2
+vx, vy = -1/2, 1/3
+w = np.asarray([x, y, vx, vy])
+E0 = Ene(w)
+
+# parameters
+h = 0.01
+tend = 12
+while t<tend:
+    #w = w + h*f(w, t)
+    # following follow NRiC (2e) eq 16.1.3
+    k1 = h*f(w, t)
+    k2 = h*f(w + 1/2*k1, t+1/2*h) # note that our w is NR's y, our t is NR's x
+    k3 = h*f(w + 1/2*k2, t+1/2*h)
+    k4 = h*f(w + k3, t+h)
+    w = w + 1/6*(k1+ 2*k2 + 2*k3 + k4)
+    t = t + h
+    E = Ene(w)
+    Eerr = (E-E0)/E0
+    #print("%3.3e %3.3e %3.3e %3.3e %3.3e %3.3e" %(t, Eerr, w[0], w[1], w[2], w[3]))
+    print("%3.3e" %(t), end=' ')
+    print("%3.3e" %(Eerr), end=' ')
+    print("%3.3e %3.3e %3.3e %3.3e" %(w[0], w[1], w[2], w[3]), end=' ')
+    print("")
+    
